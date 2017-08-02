413c45ce5f9fefb2b739ecdc8506c6015a5d9689
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-08-01 20:42:43 -0700

    basic layout

--- src/Home.js
+++ src/Home.js
@@ -14,9 +14,9 @@ class Home extends Component {
             <div className="Home-about-subheader">a bit about me</div>
           </div>
           <div className="Home-about-buttons">
-            <Circle props="resume" />
-            <Circle props="portfolio" />
-            <Circle props="skills" />
+            <div className="button-resume">My Resume</div>
+            <div className="button-portfolio">Portfolio</div>
+            <div className="button-skills">Skills</div>
           </div>
           <div className="Home-about-summary">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story and let your users get to know you.
           </div>
