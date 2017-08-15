cd5ad5c7c2e9df214e1eb80a6f33b3486084cfcc
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-08-15 16:56:13 -0700

    begin bootstrap refactor

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Resume from './Resume.js';
 import Projects from './Projects.js';
 import Contact from './Contact.js';
 import classNames from 'classnames';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class App extends Component {
 
@@ -55,37 +56,60 @@ class App extends Component {
         {currentPage: this.state.currentPage.contact}
       )
     }
-
-
     return (
-      <div className="App">
-        <div className="App-header">
-          <div className="name-title">
-            <div className="name">Nick Cobbett</div>
-            <div className="initials">NC</div>
-            <div className="title">Failed Musician/ Software Engineer</div>
-          </div>
-          <div className="drawer">
-
-            drawer
-          </div>
-          <div className="App-Navbar">
-            <div className={navItemClasses.home} onClick={this.handleNavItemClick}>Home</div>
-            <div className={navItemClasses.resume} onClick={this.handleNavItemClick}>Resume</div>
-            <div className={navItemClasses.projects} onClick={this.handleNavItemClick}>Projects</div>
-            <div className={navItemClasses.contact} onClick={this.handleNavItemClick}>Contact</div>
-          </div>
-        </div>
-        {
-          this.state.currentPage.home ? <Home className="Home" handleButtonClick={this.handleNavItemClick}/> :
-          this.state.currentPage.resume ? <Resume className="Resume"/> :
-          this.state.currentPage.projects ? <Projects className="Projects" /> :
-          this.state.currentPage.contact ? <Contact className="Contact" /> : null
-        }
-
-      </div>
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Todd Toddson</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#home">Home</NavItem>
+            <NavItem eventKey={2} href="#projects">Projects</NavItem>
+            <NavItem eventKey={3} href="#resume">Resume</NavItem>
+            <NavItem eventKey={4} href="#contact">Contact</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
+
+
+
+
+    // return (
+    //   <div className="App">
+    //     <div className="App-header">
+    //       <div className="name-title">
+    //         <div className="name">Nick Cobbett</div>
+    //         <div className="initials">NC</div>
+    //         <div className="title">Failed Musician/ Software Engineer</div>
+    //       </div>
+    //       <div className="drawer">
+
+    //         drawer
+    //       </div>
+    //       <div className="App-Navbar">
+    //         <div className={navItemClasses.home} onClick={this.handleNavItemClick}>Home</div>
+    //         <div className={navItemClasses.resume} onClick={this.handleNavItemClick}>Resume</div>
+    //         <div className={navItemClasses.projects} onClick={this.handleNavItemClick}>Projects</div>
+    //         <div className={navItemClasses.contact} onClick={this.handleNavItemClick}>Contact</div>
+    //       </div>
+    //     </div>
+    //     {
+    //       this.state.currentPage.home ? <Home className="Home" handleButtonClick={this.handleNavItemClick}/> :
+    //       this.state.currentPage.resume ? <Resume className="Resume"/> :
+    //       this.state.currentPage.projects ? <Projects className="Projects" /> :
+    //       this.state.currentPage.contact ? <Contact className="Contact" /> : null
+    //     }
+
+    //   </div>
+    // );
+
+
+
 
 export default App;
