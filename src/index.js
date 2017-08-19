2af9bcc5893296013d93f4f523f9fb4e0cff6942
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-08-19 13:24:50 -0700

    test deploy build with bootstrap

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Route, Switch, HashRouter } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import Home from './Home';
 import Projects from './Projects';
 import Resume from './Resume';
 import Contact from './Contact';
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
 
 ReactDOM.render((
     <HashRouter>
